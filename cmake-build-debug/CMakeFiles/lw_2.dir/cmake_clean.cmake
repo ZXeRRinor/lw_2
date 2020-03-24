a5995245aede74e23a80e42d45ad2625b1999db7
zxerrinor <zxerrinor@protonmail.com>
2020-03-24 16:03:29 +0300

    Initial commit.

---
+++ cmake-build-debug/CMakeFiles/lw_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lw_2.dir/main.cpp.o"
+  "lw_2"
+  "lw_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lw_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
